c41d41b977ccec5f665fdea7a2a54ee9acbe5e51
Babadu31 <bastienmiss31@gmail.com>
2022-08-31 10:43:58 +0200

    B-31/08/22-10:43-betty

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,15 +1,20 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_delete - ssdds
+ * @tree: sdsdsd
+ */
+
 void binary_tree_delete(binary_tree_t *tree)
 {
-       if (tree->left)
-       {
-             binary_tree_delete(tree->left);
-             free(tree->left);
-       }
-       if (tree->right)
-       {
-             binary_tree_delete(tree->right);
-             free(tree->right);
-       }
+if (tree->left)
+{
+binary_tree_delete(tree->left);
+free(tree->left);
+}
+if (tree->right)
+{
+binary_tree_delete(tree->right);
+free(tree->right);
+}
 }
