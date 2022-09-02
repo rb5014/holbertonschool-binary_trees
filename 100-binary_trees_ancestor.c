fbdda9e6572babffd95ed7f1ef92c4280051b79d
rb5014 <5014@holbertonschool.com>
2022-09-02 14:00:46 +0200

    100-binary_trees_ancestor.c

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+/**
+ * binary_trees_ancestors - finds the lowest
+ * common ancestor of two nodes
+ *
+ * @first: pointer to the first node
+ * @second: pointer to the second node
+ * Return: ancestor node pointer, or NULL if not found
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second)
+{
+const binary_tree_t *tmp = first, *tmp2;
+
+	while (tmp)
+	{
+		tmp2 = second;
+		if (tmp == tmp2)
+			return ((binary_tree_t *) tmp);
+		while (tmp2)
+		{
+			if (tmp2 == tmp)
+				return ((binary_tree_t *) tmp2);
+			tmp2 = tmp2->parent;
+		}
+		tmp = tmp->parent;
+	}
+	return (NULL);
+}
--- 100-main.c
+++ 100-main.c
@@ -1,5 +1,24 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
+
+/**
+ * launch_test - Test ancestor function and print informations
+ *
+ * @n1: First node
+ * @n2: Second node
+ */
+void launch_test(binary_tree_t *n1, binary_tree_t *n2)
+{
+    binary_tree_t *ancestor;
+
+    ancestor = binary_trees_ancestor(n1, n2);
+    printf("Ancestor of [%d] & [%d]: ", n1->n, n2->n);
+    if (!ancestor)
+        printf("(nil)\n");
+    else
+        printf("%d\n", ancestor->n);
+}
 
 /**
  * main - Entry point
@@ -11,18 +30,18 @@ int main(void)
     binary_tree_t *root;
 
     root = binary_tree_node(NULL, 98);
-
     root->left = binary_tree_node(root, 12);
-    root->left->left = binary_tree_node(root->left, 6);
-    root->left->right = binary_tree_node(root->left, 16);
-    root->left->left->left = binary_tree_node(root->left, 32);
-    root->left->right->right = binary_tree_node(root->left, 48);
-
     root->right = binary_tree_node(root, 402);
-    root->right->left = binary_tree_node(root->right, 256);
-    root->right->right = binary_tree_node(root->right, 512);
-    root->right->right->right = binary_tree_node(root->right, 412);
-    root->left->left->left = binary_tree_node(root->right, 615);
-
+    root->left->right = binary_tree_node(root->left, 54);
+    root->right->right = binary_tree_node(root->right, 128);
+    root->left->left = binary_tree_node(root->left, 10);
+    root->right->left = binary_tree_node(root->right, 45);
+    root->right->right->left = binary_tree_node(root->right->right, 92);
+    root->right->right->right = binary_tree_node(root->right->right, 65);
     binary_tree_print(root);
+
+    launch_test(root->left, root->right);
+    launch_test(root->right->left, root->right->right->right);
+    launch_test(root->right->right, root->right->right->right);
+    return (0);
 }
